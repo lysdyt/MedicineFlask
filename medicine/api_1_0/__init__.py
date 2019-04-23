a960de7c47b44b073cee16b91c48fd7857f57a9c
DreamCats <maifeng_cat@outlook.com>
2019-04-23 19:55:23 +0800

    create __init__.py

---
+++ medicine/api_1_0/__init__.py
@@ -0,0 +1,9 @@
+# __author__: Mai feng
+# __file_name__: __init__.py
+# __time__: 2019:04:23:19:52
+
+from flask.blueprints import Blueprint
+#需求url:127.0.0.1:5000/api/1.0/index
+api=Blueprint('api_1_0',__name__,url_prefix='/api/1.0')
+#为了能调用到视图需要导入
+from . import demo
