3cac921a0da3e515ff6ceefca4db7478e5e6da61
DreamCats <maifeng_cat@outlook.com>
2019-04-25 00:02:05 +0800

    BaseModel

--- medicine/models.py
+++ medicine/models.py
@@ -6,3 +6,8 @@
 from datetime import datetime
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+
+class BaseModel(object):
+    """模型基类"""
+    create_time=db.Column(db.DateTime,default=datetime.now()) #记录模型类创建时间
+    update_time=db.Column(db.DateTime,default=datetime.now,onupdate=datetime.now())#记录模型类更新时间
