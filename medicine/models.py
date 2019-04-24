8223e1e4306f20fd788ad7bd5a45572c00a5f680
DreamCats <maifeng_cat@outlook.com>
2019-04-24 23:56:39 +0800

    update models

--- medicine/models.py
+++ medicine/models.py
@@ -1,3 +1,8 @@
 # __author__: Mai feng
 # __file_name__: models.py
 # __time__: 2019:04:23:19:54
+
+
+from datetime import datetime
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
