49e1b71d7b16b32a2456af5573c18b1bc7e2cc8f
DreamCats <maifeng_cat@outlook.com>
2019-04-29 10:21:18 +0800

    update verify

--- medicine/api_1_0/verify.py
+++ medicine/api_1_0/verify.py
@@ -10,6 +10,7 @@ from medicine import constants
 from medicine.models import User
 import re
 from medicine.utils.captcha.captcha import captcha
+
 @api.route('/imageCode')
 def get_image_code():
     '''获取图片验证码
@@ -43,3 +44,84 @@ def get_image_code():
     response = make_response(image)
     response.headers['Content-Type'] = 'image/jpg'
     return response
+
+
+@api.route('/smsCode', methods=['POST'])
+def send_sms_code():
+    '''发送手机验证码
+    :param json
+        1. phone_num
+        2. img_code_client
+        3. uuid
+    :return: json
+    '''
+
+    # 接收参数
+    data = request.json
+    phone_num = data.get('phone_num')
+    img_code_client = data.get('img_code_client')
+    uuid = data.get('uuid')
+
+    # 检测是否缺少参数
+    if not all([phone_num, img_code_client, uuid]):
+        return jsonify(re_code=RET.PARAMERR, msg='参数缺少')
+
+    # 检查手机号格式是否正确
+    if not re.match('0?(13|14|15|17|18|19)[0-9]{9}', phone_num):
+        return jsonify(re_code=RET.PARAMERR, msg='手机号码格式不正确')
+
+     # 取出验证码，判断验证码是否正确
+    try:
+        # from redis
+        img_code_server = redis_conn.get('ImageCode' + uuid)
+    except Exception as e:
+        current_app.logger.debug(e)
+        return jsonify(re_code=RET.DBERR, msg='获取图片验证码失败')
+    
+     # 判断验证码是否已经过期
+    if not img_code_server:
+        return jsonify(re_code=RET.NODATA, msg='验证码已经过期')
+
+    # 取完就删除嘛
+    try:
+        # 从redis中删除验证码
+        redis_conn.delete('ImageCode' + uuid)
+    except Exception as e:
+        current_app.logger.debug(e)
+
+    # 判断用户输入的验证码是否正确
+    if img_code_client.lower() != img_code_server.lower():
+        return jsonify(re_code=RET.DATAERR, msg='验证码输入有误')
+
+     # 判断用户是否60秒重复发送短信验证码
+    try:
+        send_flag = redis_conn.get('SendPhoneCode' + phone_num)
+    except Exception as e:
+        current_app.logger.debug(e)
+    if send_flag is not None:
+        return jsonify(re_code=RET.REQERR, msg='请求过于频繁，请60秒后重试')
+    
+    # 判断用户是否注册
+    try:
+        user = User.query.filter(User.phone_num == phone_num).first()
+    except Exception as e:
+        current_app.logger.debug(e)
+        return jsonify(re_code=RET.DBERR, msg='查询数据库错误')
+    # 用户存在，提示用户已经被注册
+    if user:
+        return jsonify(re_code=RET.DATAEXIST, msg='该用户已经存在')
+
+     # 生成短信验证码
+    sms_code = '%06d' % random.randint(0, 99999)
+    current_app.logger.debug('短信验证码为：' + sms_code)
+
+    # 将短信验证码存储redis中
+    try:
+        redis_conn.set('PhoneCode' + phone_num, sms_code, constants.SMS_CODE_REDIS_EXPIRES)
+        redis_conn.set('SendPhoneCode'+ phone_num, 1, constants.SEND_SMS_REDIS_EXPIRES) # 验证60秒的标志
+
+    except Exception as e:
+        current_app.logger.debug(e)
+        return jsonify(re_code=RET.DBERR, msg='存储短信验证码失败')
+
+    return jsonify(re_code=RET.OK, msg='验证码发送成功')
