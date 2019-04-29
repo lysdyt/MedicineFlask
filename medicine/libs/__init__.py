b2e5694af39e775d90029f4ace8bc6459ce573e4
DreamCats <maifeng_cat@outlook.com>
2019-04-29 11:23:38 +0800

    添加云通讯库

---
+++ medicine/libs/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''
+@author:Maifeng
+@file:__init__.py.py
+@time:2019/3/168:37 PM
+
+'''
---
+++ medicine/libs/yuntongxun/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''
+@author:Maifeng
+@file:__init__.py
+@time:2019/3/185:36 PM
+
+'''
