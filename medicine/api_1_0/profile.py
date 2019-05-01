7102d5a293479d8c0e4e2e69c399168924405f8c
DreamCats <maifeng_cat@outlook.com>
2019-05-01 21:15:53 +0800

    导入包

--- medicine/api_1_0/profile.py
+++ medicine/api_1_0/profile.py
@@ -1,3 +1,10 @@
 # __author__: Mai feng
 # __file_name__: profile.py
 # __time__: 2019:05:01:20:57
+
+from medicine.api_1_0 import api
+from flask import jsonify, session, current_app, request, g
+from medicine.utils.response_code import RET
+from medicine.utils.common import login_required
+from medicine.models import User, Expert
+from medicine import db, constants
