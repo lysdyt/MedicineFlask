1efaa959fcc5fc3d94802aa75ab9e529240afd6b
DreamCats <maifeng_cat@outlook.com>
2019-05-01 21:53:54 +0800

    添加users接口

--- medicine/api_1_0/profile.py
+++ medicine/api_1_0/profile.py
@@ -8,3 +8,24 @@ from medicine.utils.response_code import RET
 from medicine.utils.common import login_required
 from medicine.models import User, Expert
 from medicine import db, constants
+
+@api.route('users')
+@login_required
+def get_user_info():
+    '''获取用户信息
+        前提是已经登陆
+    :return: json
+    '''
+    user_id = g.user_id
+    try:
+        user = User.query.filter(User.id == user_id).first()
+    except Exception as e:
+        current_app.logger.debug(e)
+        return jsonify(re_code=RET.DBERR, msg='数据库查询错我')
+
+    if not user:
+        return jsonify(re_code=RET.NODATA, msg='用户不存在')
+
+    # 查询用户信息
+    user_info = user.to_dict()
+    return jsonify(re_code=RET.OK, msg='查询成功', user=user_info)
