4906a2a865b3c561015d6c279e87d71e0c3dc3c8
DreamCats <maifeng_cat@outlook.com>
2019-05-10 21:37:35 +0800

    addEssay 接口测试完毕

--- medicine/api_1_0/essay.py
+++ medicine/api_1_0/essay.py
@@ -8,3 +8,46 @@ from medicine.utils.response_code import RET
 from medicine import redis_conn, db
 from medicine.utils.common import login_required
 from medicine.models import Essay
+
+
+@api.route('/addEssay', methods=['POST'])
+def add_essay():
+    '''添加软文
+    :param title: 文章标题
+            abstract: 文章摘要
+            # cover_img_url: 文章封面
+            content: 内容
+            types: 类型 （普通 or 商家优惠活动）
+    '''
+    data = request.json
+    title = data.get('title')
+    abstract = data.get('abstract')
+    # cover_img_url = data.get('cover_img_url')
+    content = data.get('content')
+    types = data.get('types')
+    # 判断是否缺少
+    if not all([title, abstract, content, types]):
+        return jsonify(re_code=RET.PARAMERR, msg='缺少参数')
+
+    # 判断类型参数传递是否正确
+    if types != 'common' and types != 'shop':
+        return jsonify(re_code=RET.PARAMERR, msg='types参数不正确')
+
+    # 创建实体
+    essay = Essay()
+    essay.title = title
+    essay.abstract = abstract
+    # essay.cover_img_url = cover_img_url
+    essay.content = content
+    essay.types = types
+
+    try:
+        db.session.add(essay)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.debug(e)
+        db.session.rollback()
+        return jsonify(re_code=RET.DBERR, msg='添加软文失败')
+
+    return jsonify(re_code=RET.OK, msg='添加成功')
+    
