2537181f7288dc51bef97ea997fd1d4c6dc2b060
DreamCats <maifeng_cat@outlook.com>
2019-05-14 20:27:45 +0800

    import community

--- medicine/api_1_0/__init__.py
+++ medicine/api_1_0/__init__.py
@@ -6,4 +6,5 @@ from flask.blueprints import Blueprint
 #需求url:127.0.0.1:5000/api/1.0/index
 api=Blueprint('api_1_0',__name__,url_prefix='/api/1.0')
 #为了能调用到视图需要导入
-from . import demo, passport, verify, profile, position, shop, essay, order
+from . import demo, passport, verify, profile, position, shop, essay
+from . import order, community
